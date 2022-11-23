dd3372334db48287e48d104fa2cc0abc6a353dbf
mccamara <camaramamady9160@gmail.com>
2022-11-23 17:18:26 +0000

    second

--- src/main/java/com/bezkoder/springjwt/controllers/TestController.java
+++ src/main/java/com/bezkoder/springjwt/controllers/TestController.java
@@ -1,15 +1,21 @@
 package com.bezkoder.springjwt.controllers;
 
+import com.bezkoder.springjwt.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/test")
 public class TestController {
+  @Autowired
+  private UserRepository userRepository;
   @GetMapping("/all")
   public String allAccess() {
     return "Public Content.";
@@ -17,8 +23,11 @@ public class TestController {
 
   @GetMapping("/user")
   @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
-  public String userAccess() {
-    return "User Content.";
+  public String userAccess(Principal user)
+  {
+
+    return "Bienvenue, " + userRepository.findByUsername(user.getName()).get().getUsername() +
+            userRepository.findByUsername(user.getName()).get().getRoles();
   }
 
   @GetMapping("/mod")
