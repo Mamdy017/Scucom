3dbdba4e7e25667d3086a65c349b085384760125
mccamara <camaramamady9160@gmail.com>
2022-11-24 21:35:58 +0000

    ajout de google

--- src/main/java/com/bezkoder/springjwt/controllers/AuthController.java
+++ src/main/java/com/bezkoder/springjwt/controllers/AuthController.java
@@ -20,7 +20,10 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.oidc.OidcIdToken;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -139,7 +142,7 @@ public class AuthController {
   private StringBuffer getOauth2LoginInfo(Principal user){
 
     StringBuffer protectedInfo = new StringBuffer();
-
+    OAuth2User principal = ((OAuth2AuthenticationToken) user).getPrincipal();
     OAuth2AuthenticationToken authToken = ((OAuth2AuthenticationToken) user);
     OAuth2AuthorizedClient authClient =
             this.loadAuthorizedClientService.loadAuthorizedClient(authToken.getAuthorizedClientRegistrationId(), authToken.getName());
@@ -151,6 +154,18 @@ public class AuthController {
       protectedInfo.append("Bienvenue, " + userAttributes.get("name")+"<br><br>");
       protectedInfo.append("e-mail: " + userAttributes.get("email")+"<br><br>");
       protectedInfo.append("Access Token: " + userToken+"<br><br>");
+      OidcIdToken idToken = getIdToken(principal);
+      if(idToken != null) {
+
+        protectedInfo.append("idToken value: " + idToken.getTokenValue()+"<br><br>");
+        protectedInfo.append("Token mapped values <br><br>");
+
+        Map<String, Object> claims = idToken.getClaims();
+
+        for (String key : claims.keySet()) {
+          protectedInfo.append("  " + key + ": " + claims.get(key)+"<br>");
+        }
+      }
     }
     else{
       protectedInfo.append("NA");
@@ -171,4 +186,12 @@ public class AuthController {
 
     return "Bienvenu " ;
   }*/
+
+  private OidcIdToken getIdToken(OAuth2User principal){
+    if(principal instanceof DefaultOidcUser) {
+      DefaultOidcUser oidcUser = (DefaultOidcUser)principal;
+      return oidcUser.getIdToken();
+    }
+    return null;
+  }
 }
